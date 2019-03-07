7ef3b53609c266a3c9741b9d4201c418faf22c60
Stan <koropchan.sv@gmail.com>
2019-03-06 23:41:51 -0500

    Add chart

--- src/Charts.js
+++ src/Charts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+// import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, } from 'recharts';
 
 class Charts extends Component {
 
@@ -7,47 +8,60 @@ class Charts extends Component {
         super(props);
         this.state = {
           data: [],
-        //   isLoaded: false,
         }
     }
 
     componentDidMount() {
         
-        var date = moment();
-        var formattedDate = date.format('YYYY-MM-DD');
-        var url = `https://free.currencyconverterapi.com/api/v6/convert?q=USD_CAD,PHP_USD&compact=ultra&date=2019-03-01&endDate=${formattedDate}&apiKey=95c86d5989f092030892`;
+        var currentDate = moment();
+        var weekAgoDate = moment().subtract(7, 'days');
+        var formattedCurrentDate = currentDate.format('YYYY-MM-DD');
+        var formattedWeekAgoDate = weekAgoDate.format('YYYY-MM-DD');
+
+        var url = `https://free.currencyconverterapi.com/api/v6/convert?q=USD_CAD&compact=ultra&date=${formattedWeekAgoDate}&endDate=${formattedCurrentDate}&apiKey=95c86d5989f092030892`;
         
         fetch(url)
           .then(res => res.json())
-          .then(json => {
-            this.setState({
-            //   isLoaded: true,
-              data: [json],
-            })
-          });
+          .then(d => {
+            this.setState({data: [d],});
+            console.log(this.state.data['0'].USD_CAD[1]);
+          })
+          .catch(error => console.log(error))
     }
 
     render() {
-      
-    //   var {isLoaded,data} = this.state;
-    //   if (!isLoaded){
-    //       return <div>Loading...</div>
-    //   }
-    //   else {
 
       return (
         <div>
-            <div>
             <ul>
-                {this.state.data.map(item =>(
-                    <li></li>
+                {this.state.data.map(item => (
+                    <li key= {item.id}>
+                        List: {item.name}
+                    </li>
                 ))};
             </ul>
-            </div>
         </div>
       );
-    //   }
     } 
 }
   
   export default Charts;
+
+//   <LineChart width={730} height={250} data={this.state.data}
+//   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+//   <CartesianGrid strokeDasharray="3 3" />
+//   <XAxis dataKey={this.state.data['0']} />
+//   <YAxis />
+//   <Tooltip />
+//   <Legend />
+//   <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+//   <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+//   </LineChart>
+
+
+// var json_data = this.state.data;
+//             var result = [];
+//             for(var i in json_data)
+//                 result.push([i, json_data[i]]);
+//             this.setState({data: result});
+//             console.log(this.state.data);
