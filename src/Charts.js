996c7fb143cb51b0a6f3f259c50fd83e5b6ba0f8
Stan <koropchan.sv@gmail.com>
2019-03-07 12:16:13 -0500

    Edit data from JSON, add chart

--- src/Charts.js
+++ src/Charts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-// import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, } from 'recharts';
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, } from 'recharts';
 
 class Charts extends Component {
 
@@ -23,23 +23,35 @@ class Charts extends Component {
         fetch(url)
           .then(res => res.json())
           .then(d => {
-            this.setState({data: [d],});
-            console.log(this.state.data['0'].USD_CAD[1]);
+            this.setState({data: [d]});
+
+            var dataArray = [];
+            for (const prop in this.state.data['0'].USD_CAD){
+                dataArray.push({
+                            date: prop,
+                            price: this.state.data[0].USD_CAD[prop].toString()
+                            })
+            };
+            this.setState({data: dataArray});
           })
           .catch(error => console.log(error))
+          
+          
     }
 
     render() {
-
+        
       return (
         <div>
-            <ul>
-                {this.state.data.map(item => (
-                    <li key= {item.id}>
-                        List: {item.name}
-                    </li>
-                ))};
-            </ul>
+          <LineChart width={730} height={250} data={this.state.data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="price" stroke="#8884d8" />
+          </LineChart>
         </div>
       );
     } 
@@ -47,21 +59,3 @@ class Charts extends Component {
   
   export default Charts;
 
-//   <LineChart width={730} height={250} data={this.state.data}
-//   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-//   <CartesianGrid strokeDasharray="3 3" />
-//   <XAxis dataKey={this.state.data['0']} />
-//   <YAxis />
-//   <Tooltip />
-//   <Legend />
-//   <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-//   <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-//   </LineChart>
-
-
-// var json_data = this.state.data;
-//             var result = [];
-//             for(var i in json_data)
-//                 result.push([i, json_data[i]]);
-//             this.setState({data: result});
-//             console.log(this.state.data);
